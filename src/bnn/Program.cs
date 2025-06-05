050fdf0a87304a2f0a5e4f8300251c626e9b57d8
Marcos Yanes <maeyanes@gmail.com>
2025-06-04 20:23:28 -0600

    Agrega generación de pesos aleatorios

--- src/bnn/Program.cs
+++ src/bnn/Program.cs
@@ -1,24 +1,44 @@
-﻿if (args.Length > 1)
+﻿if (args.Length > 2)
 {
-    Console.WriteLine("Must provide just one integer.");
+    Console.WriteLine("USAGE: bnn.exe [n] [seed]");
+
+    return 0;
+}
+
+int n = 10;
+int seed = Environment.TickCount;
+
+if (args.Length > 1 && !int.TryParse(args[0], out n))
+{
+    Console.WriteLine($"{args[0]} is not a valid integer.");
 
     return 1;
 }
 
-int n = 10;
-
-if (args.Length == 1 && !int.TryParse(args[0], out n))
+if (args.Length == 2 && !int.TryParse(args[1], out seed))
 {
-    Console.WriteLine($"{args[0]} is not a valid integer.");
+    Console.WriteLine($"{args[1]} is not a valid seed.");
 
-    return 2;
+    return 1;
 }
 
-Random rnd = new();
+Console.WriteLine($"Seed is {seed}");
+
+Random rnd = new(seed);
 
 for (int i = 0; i < n; i++)
 {
-    Console.WriteLine(rnd.Next(-101, 101));
+    Console.WriteLine(RandomWeight());
 }
 
 return 0;
+
+
+double RandomWeight()
+{
+    const int randMAx = 32767;
+    const int bias = randMAx / 2;
+    const double div = (randMAx + 1) * 10.0;
+
+    return (rnd.Next(0, randMAx) - bias) / div;
+}
