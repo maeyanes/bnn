b4e6a44be37d34ec1f3ff377dba9dbf768fbf776
Marcos Yanes <maeyanes@gmail.com>
2025-06-04 18:35:02 -0600

    Agrega reconocimiento de parámetros de línea de comandos

--- src/bnn/Program.cs
+++ src/bnn/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿foreach (string argument in args)
+{
+    if (int.TryParse(argument, out int _))
+    {
+        Console.WriteLine($"[I]\t{argument}");
+
+        continue;
+    }
+
+    if (double.TryParse(argument, out double _))
+    {
+        Console.WriteLine($"[R]\t{argument}");
+
+        continue;
+    }
+
+    if (char.TryParse(argument, out char _))
+    {
+        Console.WriteLine($"[C]\t{argument}");
+
+        continue;
+    }
+
+    Console.WriteLine($"[S]\t{argument}");
+}
