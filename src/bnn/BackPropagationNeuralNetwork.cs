caa65e73627dabbe134980de5bb69f0e655aa094
Marcos Yanes <maeyanes@gmail.com>
2025-06-05 19:38:29 -0600

    Agrega recibir un archivo desde un argumento o entrada estándar

---
+++ src/bnn/BackPropagationNeuralNetwork.cs
@@ -0,0 +1,160 @@
+﻿namespace bnn;
+
+public sealed class BackPropagationNeuralNetwork
+{
+    private List<List<double>> _cluster0;
+    private List<List<double>> _clusterF;
+    private int _hidden;
+    private List<double> _hiddenLayer;
+    private int _inputs;
+    private int _outputs;
+
+    public BackPropagationNeuralNetwork(int inputs = 0, int hidden = 0, int outputs = 0)
+    {
+        _inputs = inputs;
+        _hidden = hidden;
+        _outputs = outputs;
+
+        _hiddenLayer = [];
+        _cluster0 = [];
+        _clusterF = [];
+    }
+
+    public bool Get(TextReader reader)
+    {
+        string? line = reader.ReadLine();
+
+        if (line == null)
+        {
+            return false;
+        }
+
+        string[]? header = line.Split([' ', '\t'], StringSplitOptions.RemoveEmptyEntries);
+
+        if (header.Length < 3)
+        {
+            return false;
+        }
+
+        if (!int.TryParse(header[0], out int inputs) || !int.TryParse(header[1], out int hidden) ||
+            !int.TryParse(header[2], out int outputs))
+        {
+            return false;
+        }
+
+        int weights = inputs + 1;
+
+        List<List<double>> cluster0 = new(weights);
+
+        for (int r = 0; r < hidden; r++)
+        {
+            List<double> neuron = ReadLineOfDoubles(reader, weights);
+
+            if (neuron.Count == 0)
+            {
+                return false;
+            }
+
+            cluster0.Add(neuron);
+        }
+
+        weights = hidden + 1;
+
+        List<List<double>> clusterF = [];
+
+        for (int r = 0; r < outputs; r++)
+        {
+            List<double> neuron = ReadLineOfDoubles(reader, weights);
+
+            if (neuron.Count == 0)
+            {
+                return false;
+            }
+
+            clusterF.Add(neuron);
+        }
+
+        _inputs = inputs;
+        _hidden = hidden;
+        _outputs = outputs;
+        _cluster0 = cluster0;
+        _clusterF = clusterF;
+
+        return true;
+    }
+
+    public void Put(TextWriter writer)
+    {
+        writer.WriteLine($"{_inputs} {_hidden} {_outputs}");
+
+        Show(writer);
+    }
+
+    public void Show(TextWriter writer)
+    {
+        int weights = _inputs + 1;
+
+        for (int n = 0; n < _hidden; n++)
+        {
+            for (int w = 0; w < weights; w++)
+            {
+                if (w > 0)
+                {
+                    writer.Write('\t');
+                }
+
+                writer.Write(_cluster0[n][w]);
+            }
+
+            writer.WriteLine();
+        }
+
+        weights = _hidden + 1;
+
+        for (int n = 0; n < _outputs; n++)
+        {
+            for (int w = 0; w < weights; w++)
+            {
+                if (w > 0)
+                {
+                    writer.Write('\t');
+                }
+
+                writer.Write(_clusterF[n][w]);
+            }
+
+            writer.WriteLine();
+        }
+    }
+
+    private List<double> ReadLineOfDoubles(TextReader reader, int expectedCount)
+    {
+        string? line = reader.ReadLine();
+
+        if (line == null)
+        {
+            return Enumerable.Empty<double>().ToList();
+        }
+
+        string[] tokens = line.Split([' ', '\t'], StringSplitOptions.RemoveEmptyEntries);
+
+        if (tokens.Length != expectedCount)
+        {
+            return Enumerable.Empty<double>().ToList();
+        }
+
+        List<double> result = [];
+
+        foreach (string token in tokens)
+        {
+            if (!double.TryParse(token, out double item))
+            {
+                return Enumerable.Empty<double>().ToList();
+            }
+
+            result.Add(item);
+        }
+
+        return result;
+    }
+}
--- src/bnn/Program.cs
+++ src/bnn/Program.cs
@@ -1,88 +1,50 @@
-﻿if (args.Length > 4 || args.Length < 3)
-{
-    Console.WriteLine("USAGE: bnn.exe INPUTS HIDDEN OUTPUTS [seed]");
+﻿using bnn;
 
-    return 0;
+TextReader reader;
+
+if (Console.IsInputRedirected)
+{
+    reader = Console.In;
+}
+else
+{
+    if (args.Length != 1)
+    {
+        Console.WriteLine("USAGE: bnn.exe WEIGHTS_FILE");
+
+        return 0;
+    }
+
+    string filename = args[0];
+
+    if (!File.Exists(filename))
+    {
+        Console.WriteLine($"ERROR: File '{filename}' does not exist.");
+
+        return 1;
+    }
+
+    reader = new StreamReader(filename);
 }
 
-int seed = Environment.TickCount;
+BackPropagationNeuralNetwork bnn = new();
 
-if (!int.TryParse(args[0], out int inputs))
+if (!bnn.Get(reader))
 {
-    Console.WriteLine($"{args[0]} is not a valid integer.");
-
-    return 1;
-}
-
-if (!int.TryParse(args[1], out int hidden))
-{
-    Console.WriteLine($"{args[1]} is not a valid integer.");
+    Console.WriteLine("ERROR: Bad neural network weights data.");
 
     return 2;
 }
 
-if (!int.TryParse(args[2], out int outputs))
-{
-    Console.WriteLine($"{args[2]} is not a valid integer.");
-
-    return 3;
-}
-
-
-if (args.Length > 3 && !int.TryParse(args[3], out seed))
-{
-    Console.WriteLine($"{args[3]} is not a valid seed.");
-
-    return 4;
-}
-
-Console.WriteLine($"Seed is {seed}");
-Console.WriteLine($"{inputs} {hidden} {outputs}");
-
-Random rnd = new(seed);
-
-int weights = inputs + 1;
-
-for (int r = 0; r < hidden; ++r)
-{
-    for (int c = 0; c < weights; ++c)
-    {
-        if (c > 0)
-        {
-            Console.Write("\t");
-        }
-
-        Console.Write($"{RandomWeight()} ");
-    }
-
-    Console.WriteLine();
-}
-
-weights = hidden + 1;
-
-for (int r = 0; r < outputs; r++)
-{
-    for (int c = 0; c < weights; c++)
-    {
-        if (c > 0)
-        {
-            Console.Write("\t");
-        }
-
-        Console.Write($"{RandomWeight()} ");
-    }
-
-    Console.WriteLine();
-}
+bnn.Show(Console.Out);
 
 return 0;
 
-
-double RandomWeight()
-{
-    const int randMAx = 32767;
-    const int bias = randMAx / 2;
-    const double div = (randMAx + 1) * 10.0;
-
-    return (rnd.Next(0, randMAx) - bias) / div;
-}
+// double RandomWeight()
+// {
+//     const int randMAx = 32767;
+//     const int bias = randMAx / 2;
+//     const double div = (randMAx + 1) * 10.0;
+//
+//     return (rnd.Next(0, randMAx) - bias) / div;
+// }
